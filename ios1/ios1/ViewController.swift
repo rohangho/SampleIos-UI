a687a7c648ffefffbfdb7edc9a295db38fa4c77e
rohangho <roha@marsplay.co>
2020-05-13 23:50:15 +0530

    Added the Ios Demo 2

---
+++ ios1/ios1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios1
+//
+//  Created by Rohan Ghosh on 10/05/20.
+//  Copyright © 2020 Rohan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
